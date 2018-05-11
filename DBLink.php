1970e03ce7a0880858ef7f4b87b02243f2e38d67
Kendall-Marshall <kmanmarshall@yahoo.com>
2018-05-11 23:20:06 +1200

    Added webhook

--- DBLink.php
+++ DBLink.php
@@ -25,16 +25,41 @@
    </thead>
    <tbody>
 <?php
-$query = "SELECT * FROM papers";
-$result = $db->query($query);
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo "<tr>";
-    echo "<td>" . $row["papercode"] . "</td>";
-    echo "<td>" . $row["papername"] . "</td>";
-    echo "<td>" . $row["prereq"] . "</td>";
-    echo "</tr>";
-}
-$result->closeCursor();
+    $query = "SELECT * FROM papers";
+    $result = $db->query($query);
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        echo "<tr>";
+        echo "<td>" . $row["papercode"] . "</td>";
+        echo "<td>" . $row["papername"] . "</td>";
+        echo "<td>" . $row["prereq"] . "</td>";
+        echo "</tr>";
+    }
+    $result->closeCursor();
+
+    function processMessage($update) {
+        //$pre = setupDB($paper);
+        if($update["result"]["action"] == "sayHello" ){
+            $paper = $update["result"]["parameters"]["paperName1"];
+            
+            
+            sendMessage(array(
+                "source" => $update["result"]["source"],
+                "speech" => "Hello from webhook " . $paper,
+                "displayText" => "Hello from webhook "  . $paper,
+                "contextOut" => array()
+            ));
+        }
+    }
+
+    function sendMessage($parameters) {
+        echo json_encode($parameters);
+    }
+
+    $update_response = file_get_contents("php://input");
+    $update = json_decode($update_response, true);
+    if (isset($update["result"]["action"])) {
+        processMessage($update);
+    }
 ?>
    </tbody>
   </table>
---
+++ webhook.php
@@ -0,0 +1,27 @@
+<?php
+    echo "hello";
+    function processMessage($update) {
+            //$pre = setupDB($paper);
+            if($update["result"]["action"] == "sayHello" ){
+                $paper = $update["result"]["parameters"]["paperName1"];
+                
+                
+                sendMessage(array(
+                    "source" => $update["result"]["source"],
+                    "speech" => "Hello from webhook " . $paper,
+                    "displayText" => "Hello from webhook "  . $paper,
+                    "contextOut" => array()
+                ));
+            }
+        }
+        
+        function sendMessage($parameters) {
+            echo json_encode($parameters);
+        }
+        
+        $update_response = file_get_contents("php://input");
+        $update = json_decode($update_response, true);
+        if (isset($update["result"]["action"])) {
+            processMessage($update);
+        }
+?>
