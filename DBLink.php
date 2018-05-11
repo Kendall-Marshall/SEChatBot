7c4ba14bfb7e710ecc5c9b0d0bf215d3ec08d37d
Kendall-Marshall <kmanmarshall@yahoo.com>
2018-05-11 23:10:00 +1200

    Added DB lINK

---
+++ DBLink.php
@@ -0,0 +1,42 @@
+<?php
+    $dsn = "pgsql:"
+    . "host=ec2-23-23-248-192.compute-1.amazonaws.com;"
+    . "dbname=d7l6p49ppkecvu;"
+    . "user=ggxtnsrwvguhht;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=9553b589643fe647133277e9a8cf1bd74a6e2cac8ecbc377a0d92a53a3fcd6d7";
+
+    $db = new PDO($dsn);
+?>
+
+<html>
+ <head>
+  <title>Employees</title>
+ </head>
+ <body>
+  <table>
+   <thead>
+    <tr>
+     <th>CODE</th>
+     <th>NAem</th>
+     <th>Pre</th>
+    </tr>
+   </thead>
+   <tbody>
+<?php
+$query = "SELECT * FROM papers";
+$result = $db->query($query);
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    echo "<tr>";
+    echo "<td>" . $row["papercode"] . "</td>";
+    echo "<td>" . $row["papername"] . "</td>";
+    echo "<td>" . $row["prereq"] . "</td>";
+    echo "</tr>";
+}
+$result->closeCursor();
+?>
+   </tbody>
+  </table>
+ </body>
+</html>
